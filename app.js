9fd7ae477dcc72dcf2406fc92e4b7cf1688e611b
lou-pastorino <168196916+lou-pastorino@users.noreply.github.com>
2024-05-23 12:47:27 +0100

    Added categories for quote

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const apiUrl = "https://api.quotable.io/random/";
+const wiseApiUrl = "https://api.quotable.io/random?tags=wisdom";
+const famousApiUrl = "https://api.quotable.io/random?tags=famous-quotes";
 
 const generatorButton = document.getElementById("generate");
 const tweetButton = document.getElementById("tweet-btn");
+const wiseQuoteBtn = document.getElementById("wisdom");
+const famousQuoteBtn = document.getElementById("famous");
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
 getQuote = async (url) => {
   const response = await fetch(url);
   let data = await response.json();
-  console.log(data);
+  //   console.log(data);
   quote.innerText = data.content;
   author.innerText = data.author;
 };
@@ -29,4 +33,11 @@ generatorButton.addEventListener("click", () => {
 
 tweetButton.addEventListener("click", (query) => {
   tweet();
+});
+
+wiseQuoteBtn.addEventListener("click", () => {
+  getQuote(wiseApiUrl);
+});
+famousQuoteBtn.addEventListener("click", () => {
+  getQuote(famousApiUrl);
 });
